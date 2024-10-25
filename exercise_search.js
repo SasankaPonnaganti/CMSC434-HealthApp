d34dc3f83fedbf3672c75ae8c61d6f4119bb5e57
riyathapar <riyathapar06@gmail.com>
2024-10-24 22:58:26 -0400

    more excercises

--- exercise_search.js
+++ exercise_search.js
@@ -1,9 +1,18 @@
 function filter() {
-    group = document.getElementById("muscle-group").value;
-    for (i of document.getElementsByClassName("any")) {
+    let group = document.getElementById("muscle-group").value;
+    let equipment = document.getElementById("equipment").value;
+    let all = document.getElementsByClassName("any-m");
+    for (let i of all) {
         i.style.display = "none";
     }
-    for (i of document.getElementsByClassName(group)) {
-        i.style.display = "block";
+    let groupElements = Array.from(document.getElementsByClassName(group));
+    let equipmentElements = Array.from(document.getElementsByClassName(equipment));
+    if (groupElements.filter((element) => equipmentElements.includes(element)).length == 0) {
+        document.getElementById("nothing-found").style.display = "block";
+    } else {
+        for (let i of groupElements.filter((element) => equipmentElements.includes(element))) {
+            i.style.display = "block";
+        }
+        document.getElementById("nothing-found").style.display = "none";
     }
 }
